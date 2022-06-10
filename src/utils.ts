68aa0b91ead5c64210bfd93507c03dd4b978caa7
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-06-10 13:28:13 -0500

    hard coded service name

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,5 @@
-import { readFileSync } from 'fs';
-import { parse } from 'yaml';
-
 export function getServiceName() {
-  const data = readFileSync('./app.yaml', 'utf8');
-  const { service } = parse(data);
-  if (!service) {
-    throw new Error('Could not find service name in app.yaml');
-  }
-  return service as string;
+  return 'nft-transfers';
 }
 
 export function sleep(duration: number): Promise<void> {
