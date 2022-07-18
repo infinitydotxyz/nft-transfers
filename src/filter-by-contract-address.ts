505ef8932ae611e8fbcc7ea22f31bb692f9e8ba8
nneverlander <findadit@gmail.com>
2022-07-18 16:08:09 +0200

    fix

--- src/filter-by-contract-address.ts
+++ src/filter-by-contract-address.ts
@@ -16,13 +16,14 @@ export const filterByContractAddress =
     const from = trimLowerCase(tx.from);
     const to = trimLowerCase(tx.to);
 
-    const decodedData = infinityExchangeIface.parseTransaction({ data: tx.data, value: tx.value });
-    const functionName = decodedData.name;
-
     if (options.blockList.has(from) || options.blockList.has(to)) {
       // should still handle the case where Infinity Exchange is used to transfer/batch transfer NFTs via its helper function
-      if (to === ETHEREUM_INFINITY_EXCHANGE_ADDRESS && functionName === INFINITY_TRANSFER_NFTS_FUNCTION_NAME) {
-        return true;
+      if (to === ETHEREUM_INFINITY_EXCHANGE_ADDRESS) {
+        const decodedData = infinityExchangeIface.parseTransaction({ data: tx.data, value: tx.value });
+        const functionName = decodedData.name;
+        if (functionName === INFINITY_TRANSFER_NFTS_FUNCTION_NAME) {
+          return true;
+        }
       } else {
         console.log(`Filtered out transfer ${transfer.txHash}`);
         return false;
