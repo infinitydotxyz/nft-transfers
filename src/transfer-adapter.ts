6e367cd04ea4d28f5f1ec1da19cd42dafad80ab9
nneverlander <findadit@gmail.com>
2022-07-18 14:40:25 +0200

    fix

--- src/transfer-adapter.ts
+++ src/transfer-adapter.ts
@@ -1,5 +1,4 @@
 import { ChainId, TokenStandard } from '@infinityxyz/lib/types/core';
-import { NULL_ADDRESS } from '@infinityxyz/lib/utils';
 import { trimLowerCase } from '@infinityxyz/lib/utils/formatters';
 import { BigNumber, ethers } from 'ethers';
 import { TransferEventType, Transfer, TransferLog } from 'types/transfer';
@@ -7,23 +6,17 @@ import { TransferEventType, Transfer, TransferLog } from 'types/transfer';
 export function erc721TransferLogAdapter(transfer: TransferLog, type: TransferEventType): Transfer {
   const topics = transfer.topics;
 
-  let from = trimLowerCase(ethers.utils.hexValue(topics[1]));
-  let to = trimLowerCase(ethers.utils.hexValue(topics[2]));
-  if (from === '0x0') {
-    from = NULL_ADDRESS;
-  }
-  if (to === '0x0') {
-    to = NULL_ADDRESS;
-  }
+  const from = trimLowerCase(ethers.utils.hexZeroPad(ethers.utils.hexValue(topics[1]), 20));
+  const to = trimLowerCase(ethers.utils.hexZeroPad(ethers.utils.hexValue(topics[2]), 20));
 
-  const address = trimLowerCase(transfer.address);
+  const tokenAddress = trimLowerCase(transfer.address);
   const tokenId = BigNumber.from(topics[3]).toBigInt().toString();
 
   return {
     txHash: transfer.transactionHash,
     from,
     to,
-    address,
+    address: tokenAddress,
     chainId: ChainId.Mainnet, // TODO support other chains
     tokenId,
     blockNumber: transfer.blockNumber,
